a7fcea98bbf8bb91db738512088dffa0b1ed491b
DIALLO <amadou9393@yahoo.fr>
2021-12-04 12:01:38 +0000

    started screen

--- Screens/Started.js
+++ Screens/Started.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 
 const Started = () => {
     return (
-        <View>
-            <Text>me</Text>
+        <View >
+            <Image style={{height:400}} source={require("../assets/start.png")}/>
+            <View style={{marginTop:50,marginHorizontal:20}}>
+                <Text style={{fontSize:20,fontWeight:"bold"}}>KEEP IN TOUCH WITH THE PEOPLE AMPERSAND </Text>
+                <Text style={{fontStyle:"italic"}}> Sign in or register with your ampersand email</Text>
+            </View>
+            <View style={{flexDirection:"row",justifyContent:"space-between",marginHorizontal:25,marginTop:150}}>
+                <View>
+                    <TouchableOpacity>
+                     <Text style={{borderBottomWidth:2,borderColor:"red"}}>REGISTER</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                <TouchableOpacity>
+                <Text style={{borderBottomWidth:2,borderColor:"red"}}>SIGN IN</Text>
+                        </TouchableOpacity>
+                </View>
+            </View>
+           
+           
         </View>
     );
 }
